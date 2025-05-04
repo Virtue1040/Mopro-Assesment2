c59da29ac8d67bf23acf5daac62905b6df105d00
Rafi Hidayat <rafihidayat@student.telkomuniversity.ac.id>
2025-05-04 20:31:27 +0700

    Hapus warning filterchip

--- app/src/main/java/com/rafi607062330092/assesment2/screen/DetailScreen.kt
+++ app/src/main/java/com/rafi607062330092/assesment2/screen/DetailScreen.kt
@@ -16,8 +16,6 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
